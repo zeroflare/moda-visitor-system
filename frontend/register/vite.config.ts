307fe636b9c6dbca624a75bfc35828e74d44c6e3
Charlie <collon40408@gmail.com>
2025-11-28 16:04:41 +0800

    feat: initial shadcn/ui

--- frontend/register/vite.config.ts
+++ frontend/register/vite.config.ts
@@ -1,10 +1,16 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     outDir: path.resolve(__dirname, '../../backend/public/register'),
     emptyOutDir: true,
