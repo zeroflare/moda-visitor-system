f6284914a6a473cdd094a86ae230a13a991ce4b2
周詳 <hchou@moda.gov.tw>
2025-11-28 11:05:45 +0800

    Update all

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import messageRoute from "./message.route";
 import checkinRoute from "./checkin.route";
+import registerRoute from "./register.route";
 
 const apiRoutes = new Hono<{ Bindings: CloudflareBindings }>();
 
 apiRoutes.route("/message", messageRoute);
 apiRoutes.route("/checkin", checkinRoute);
+apiRoutes.route("/register", registerRoute);
 
 export default apiRoutes;
 
--- backend/src/types/env.d.ts
+++ backend/src/types/env.d.ts
@@ -3,7 +3,12 @@ declare interface CloudflareBindings {
   TWDIW_VP_ID: string;
   TWDIW_VP_ID_CHECKIN: string;
   TWDIW_VP_TOKEN: string;
+  TWDIW_VC_URL: string;
+  TWDIW_VC_TOKEN: string;
+  TWDIW_VC_ID: string;
   PRIVATE_KEY: string;
-  CODE_KV: KVNamespace;
+  KV: KVNamespace;
+  MAILGUN_API_KEY: string;
+  MAILGUN_DOMAIN: string;
 }
 
